45da128522113a9ada348c032d70670b2f2d3d89
KYEONGMIN CHO <ggingmin@gmail.com>
2023-04-07 16:10:42 +0900

    first

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import pkg from '@slack/bolt';
-import openai from 'openai';
-const { App, HTTPReceiver } = pkg;
+const { App } = pkg;
+import { Configuration, OpenAIApi } from "openai";
 
 
-openai.apiKey = process.env.OPENAI_API_KEY;
-
-const httpReceiver = new HTTPReceiver({
-    unhandledRequestHandler: (args) => { console.warn('uh oh unhandled request'); }
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
+
+const openai = new OpenAIApi(configuration);
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  receiver: httpReceiver,
-  signatureVerification: false,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
 });
 
-const generateResponse = async (text) => {
-  const response = await openai.complete({
-    engine: 'davinci',
-    prompt: text,
-    maxTokens: 60,
-  });
-  return response.choices[0].text.trim();
-};
 
-app.message(async ({ message, say }) => {
-  try {
-    const response = await generateResponse(message.text);
-    await say(response);
-  } catch (error) {
-    console.error(error);
-  }
+app.message('Test', async ({ message, say }) => {
+  await say(`Test: <@${message.user}>`);
+});
+
+app.event("message", async ({ event, say }) => {
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${event.text}`,
+    temperature: 0.9,
+    max_tokens: 300,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0.6,
+    stop: [" Human:", " AI:"],
+  });
+
+  console.log(response);
+  say(response.data.choices[0].text);
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log('ChatGPT bot is running!');
+  await app.start();
+
+  console.log('⚡️ Bolt app is running!');
 })();
